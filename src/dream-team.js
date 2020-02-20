ea94c65f6a57e1f9d6b2447cc2ad9a23799debe4
MikhailPakaliuk <mr.dyurilen@mail.ru>
2020-02-20 09:36:56 +0300

    feat: function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (typeof members !== "object" || members == null) {
+    return false;
+  }
+  if (members.length === undefined) {
+    return false;
+  }
+  members = members.filter(function (a) { return typeof a == 'string' });
+  members = members.map(a => a.trim());
+  let resultArray = members.map((a) => a[0].toLocaleUpperCase());
+  let result = resultArray.sort((a, b) => a.charCodeAt() - b.charCodeAt()).join('');
+  return result;
+}; 
