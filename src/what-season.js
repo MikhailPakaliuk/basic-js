36651ee23bf500d17e5a3b4721eb4590950f799c
MikhailPakaliuk <mr.dyurilen@mail.ru>
2020-03-19 14:40:59 +0300

    feat: function getSeason
    do not pass tests "throws an error on a very tricky moment"

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,32 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let result;
+  if (date === undefined) {
+    result = 'Unable to determine the time of year!';
+  } else {
+    switch (date.getMonth()) {
+      case 11:
+      case 0:
+      case 1:
+        result = 'winter';
+        break;
+      case 2:
+      case 3:
+      case 4:
+        result = 'spring';
+        break;
+      case 5:
+      case 6:
+      case 7:
+        result = 'summer';
+        break;
+      case 8:
+      case 9:
+      case 10:
+        result = 'autumn';
+        break;
+      default:
+        result = 'ololo';
+    }
+  }
+  return result;
 };
